6ff7dde711c274b2cb9c313d3652873a50cd0cc6
devhunter1 <anubhabkundu2@gmail.com>
2025-03-22 13:58:44 +0530

    a14x: switch to python extract utils

---
+++ extract-files.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2025 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/samsung/universal8535-common',
+]
+
+module = ExtractUtilsModule(
+    'a14x',
+    'samsung',
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'universal8535-common', module.vendor
+    )
+    utils.run()
