5e1bfb9605c065bef557ad4879ddb72d2033f6b1
devhunter1 <anubhabkundu2@gmail.com>
2025-06-22 10:23:55 +0530

    a14x: update blobs from A146BXXU8DYF4
    Change-Id: Iee0ee196c745b1016fefb1aef9b76444e5bd11fa

--- extract-files.py
+++ extract-files.py
@@ -35,6 +35,14 @@ lib_fixups: lib_fixups_user_type = {
 } # fmt: skip
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/lib64/libskeymint_cli.so': blob_fixup()
+        .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
+   ( 
+       'vendor/lib64/libskeymint10device.so',
+       'vendor/bin/hw/android.hardware.security.keymint-service.samsung',
+   ): blob_fixup()
+        .add_needed('android.hardware.security.rkp-V3-ndk.so')
+        .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
    (
        'vendor/lib/libexynosgraphicbuffer.so',
        'vendor/lib64/libexynosgraphicbuffer.so',
