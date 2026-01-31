e920d0ba9cac3b437213a6bece63c059c8a1f216
devhunter1 <anubhabkundu2@gmail.com>
2026-01-31 09:05:59 +0530

    a14x: Update blobs from A146BXXUBDYL1
    - Also drop the build finger print props
    Change-Id: I255ed756f3d8f25e65407d9f726170c50c914ee4

--- lineage_a14x.mk
+++ lineage_a14x.mk
@@ -36,9 +36,3 @@ PRODUCT_MODEL := SM-A146B
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="a14xxx-user 15 AP3A.240905.015.A2 A146BXXUADYJ2 release-keys" \
-    BuildFingerprint=samsung/a14xxx/essi:15/AP3A.240905.015.A2/A146BXXUADYJ2:user/release-keys \
-    DeviceProduct=a14xxx \
-    SystemName=a14xxx
