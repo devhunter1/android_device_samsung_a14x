18ac85f7bcc7e2c49018879d80212826091d553c
devhunter1 <anubhabkundu2@gmail.com>
2026-04-04 20:53:59 +0530

    a14x: Merge mixer_gains and mixer_path xml
    - Needed for oss libaudiotoute
    
    Change-Id: Ia42c79c9d73d99fb3904f889e27ab8b400247a49

--- device.mk
+++ device.mk
@@ -26,8 +26,7 @@ $(call inherit-product, vendor/samsung/a14x/a14x-vendor.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    $(DEVICE_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml
+    $(DEVICE_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Copy kernel firmare to recovery
 PRODUCT_COPY_FILES += \
